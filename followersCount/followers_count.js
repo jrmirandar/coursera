0d5921944cc81bfd96809ecad82ce23a5f8831c6
Jonathan Miranda <jrmiranddare@gmail.com>
2025-09-19 01:38:46 -0400

    FInciones de control

---
+++ followersCount/followers_count.js
@@ -0,0 +1,19 @@
+let count = 0;
+
+function increaseCount(){
+    count++;
+    displayCount();
+    checkCountValue();
+}
+
+function displayCount(){
+    document.getElementById('countDisplay').innerHTML=count;
+}
+
+function checkCountValue(){
+    if(count == 10){
+        alert("¡Tu publicación ganó 10 seguidores! ¡Felicidades!");
+    } else if(count ==20) {
+        alert("¡Tu publicación ganó 20 seguidores! ¡Sigue así!");
+    }
+}
